17dbc072c9eb89233f491007bad1e0404b7021bb
Raymond Cavalcante <rcavalca@umich.edu>
2024-03-20 14:15:20 -0400

    Fix data copy for repo initialization

--- repo_setup.sh
+++ repo_setup.sh
@@ -1,3 +1,3 @@
 # Setup input directory, assuming you're at repo root
 mkdir -p source/inputs
-cp -rl /nfs/mm-isilon/bioinfcore/ActiveProjects/rcavalca/scworkshop_code_testing/ISC_R/inputs/10x_analysis source/inputs
+cp -rl /nfs/mm-isilon/bioinfcore/ActiveProjects/rcavalca/scworkshop_code_testing/ISC_R/inputs/10x_mat_filtered source/inputs
