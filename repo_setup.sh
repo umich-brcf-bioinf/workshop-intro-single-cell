1b1312e8fb500a842893f360bded0a375378e067
Dana King <dkcoxie@gmail.com>
2025-01-30 14:54:17 -0500

    Fixed copy command for untracked input files

--- repo_setup.sh
+++ repo_setup.sh
@@ -3,5 +3,5 @@ mkdir -p source/inputs
 
 ## Pull lastest version of inputs (with updated CellRanger) - 1.28.25
 ## Note that path to input data will be different if we're knitting on the AWS server
-cp -r /nfs/mm-isilon/bioinfcore/ActiveProjects/workshop/inputs source/inputs 
+cp -r /nfs/mm-isilon/bioinfcore/ActiveProjects/workshop/inputs/* source/inputs 
 # -l flag triggered failure for updated input data 
