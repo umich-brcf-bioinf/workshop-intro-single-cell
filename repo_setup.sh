952ea2340b567c5521fa69389682ac1943957685
dkcoxie <damki@umich.edu>
2025-06-19 13:02:08 -0400

    Revised setup to match directory names and location for last workshop

--- repo_setup.sh
+++ repo_setup.sh
@@ -2,10 +2,8 @@
 mkdir -p source/inputs/prepared_data
 
 ## Pull lastest version of inputs (with updated CellRanger) - 1.28.25
-## Note that path to input data will be different if we're knitting on the AWS server
-## File versions will need to be manually updated to match versions on AWS
-cp -r /nfs/mm-isilon/bioinfcore/ActiveProjects/workshop/inputs/* source/inputs 
+#cp -r /nfs/mm-isilon/bioinfcore/ActiveProjects/workshop/inputs/* source/inputs 
 
-## On AWS - fFle versions depend on syncing source files
+## On AWS - file versions depend on syncing source files from core storage 
 cp -r ~/ISC_R/inputs/10x_cellranger_filtered_triples source/inputs/
-cp -r ~/ISC_R/inputs/prepared_data/phenos.csv source/inputs/prepared_data/
+cp -r ~/ISC_R/inputs/prepared_data/*.csv source/inputs/prepared_data/
