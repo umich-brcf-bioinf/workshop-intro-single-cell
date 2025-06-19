4a09a9db4f92089750d13261f76b120277424703
dkcoxie <damki@umich.edu>
2025-06-19 16:02:08 -0400

    Corrected paths to match naming used for last workshop to pull inputs on AWS

--- repo_setup.sh
+++ repo_setup.sh
@@ -1,9 +1,10 @@
 ## Setup input directory, assuming you're at repo root
 mkdir -p source/inputs/prepared_data
 
-## Pull lastest version of inputs (with updated CellRanger) - 1.28.25
+## Pulls lastest version of inputs (with updated CellRanger) - 1.28.25
 #cp -r /nfs/mm-isilon/bioinfcore/ActiveProjects/workshop/inputs/* source/inputs 
 
-## On AWS - file versions depend on syncing source files from core storage 
+## On AWS - file versions depend on syncing source files from core storage if any changes since previous workshop
 cp -r ~/ISC_R/inputs/10x_cellranger_filtered_triples source/inputs/
 cp -r ~/ISC_R/inputs/prepared_data/*.csv source/inputs/prepared_data/
+cp -r ~/ISC_R/inputs/prepared_data/rdata source/inputs/prepared_data/
