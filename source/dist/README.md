56a70074c320a15bf2c1c6bda8fdab26b415d7a9
Chris Gates <cgates@umich.edu>
2025-07-09 14:34:28 -0400

    Add memory management and cluster faq

---
+++ source/dist/README.md
@@ -0,0 +1,15 @@
+# Dbox - an Image Lightbox Using HTML5 Dialog Element  
+
+a lightbox script written in es6 using the [dialog element](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog) and [Color Thief](http://lokeshdhakar.com/projects/color-thief/) to apply the images dominant color to the lightbox background.   
+
+**object-fit is used to display images in the lightbox. This isn't supported everywhere. see [Post on Usage: Limitations](https://corydowdy.com/blog/dbox-an-image-lightbox-using-html5-dialog-element#limitations) for more info** 
+
+# Examples and How To Use:  
+* basic: https://cdowdy.github.io/dialog-lightbox/ 
+* modal: https://cdowdy.github.io/dialog-lightbox/dbox-modal.html  
+* change classes or ID: https://cdowdy.github.io/dialog-lightbox/dbox-classes-id.html  
+
+# Post on usage:  
+https://corydowdy.com/blog/dbox-an-image-lightbox-using-html5-dialog-element
+
+
--- source/images/cluster_matrix/matrix.R
+++ source/images/cluster_matrix/matrix.R
@@ -1,31 +1,52 @@
+# -------------------------------------------------------------------------
+# matrix.R
+# Generates and saves UMAPs and csvs for a matrix of PCs and resolutions
+# In addition to the cross-product of resolutions x PCs also generates
+#   a "blank" set of blank UMAPs without clusters.
+# Takes "a good while" to run
+# Overwrites any existing files; does not delete any existing files.
+# cgates 7/3/2025
+
 library(Seurat)
 library(tidyverse)
 
-geo_so = readRDS('~/ISC_R/inputs/prepared_data/rdata/geo_so_sct_clustered.rds')
+output_dir = 'source/images/cluster_matrix/'
+geo_so = readRDS('~/ISC_R/inputs/prepared_data/rdata/geo_so_sct_integrated.rds')
+
+resolutions = c(0.2, 0.4, 0.8, 1.2)
+included_pcs = c(5, 10 , 16, 30)
 
 # -------------------------------------------------------------------------
-resolutions = c(0.2, 0.4, 0.8, 1.2, 1.6)
-included_pcs = c(5, 10 , 25, 50)
 
-# get_data = function (object, group.by, reduction, dims = c(1,2)) {
-#   cells <- Cells(x = object, assay = DefaultAssay(object = object[[reduction]]))
-#   dims <- paste0(Key(object = object[[reduction]]), dims)
-#   orig.groups <- group.by
-#   group.by <- group.by %||% 'ident'
-#   data <- FetchData(
-#     object = object,
-#     vars = c(dims, group.by),
-#     cells = cells,
-#     clean = 'project')
-#   return (data)
-# }
+# Initialize a table of stats with the "blank", res=0 results
+pc_res_cluster = tibble(expand.grid(included_pcs = included_pcs,
+                                    resolution = 0,
+                                    cluster_count=0)) %>% 
+  mutate(umap_filename=sprintf("umap/res=0_pcs=%02d.png", included_pcs))
 
-pc_res_cluster = tibble(num_pcs = numeric(), resolution= numeric(), cluster_count=numeric())
 
+# -------------------------------------------------------------------------
+# Extracts the cluster data (UMAP1, UMAP2) from a Seurat Object reduction
+get_data = function (object, group.by, reduction, dims = c(1,2)) {
+  cells <- Cells(x = object, assay = DefaultAssay(object = object[[reduction]]))
+  dims <- paste0(Key(object = object[[reduction]]), dims)
+  orig.groups <- group.by
+  group.by <- group.by %||% 'ident'
+  data <- FetchData(
+    object = object,
+    vars = c(dims, group.by),
+    cells = cells,
+    clean = 'project')
+  return (data)
+}
+
+
+# -------------------------------------------------------------------------
+# Run through the combination of PCs and res and generate the UMAPs
 for (pc in included_pcs) { 
   for(res in resolutions) {
-    name_suffix = sprintf('pcs=%s_res=%s', pc, res)
-    umap_file = sprintf('results/figures/cluster_matrix_%s.png', name_suffix)
+    name_suffix = sprintf('res=%s_pcs=%02d', res, pc)
+    umap_file = sprintf('umaps/%s.png', name_suffix)
     cluster_column = sprintf('SCT_snn_res.%s', res)
     
     message(name_suffix)
@@ -35,35 +56,99 @@ for (pc in included_pcs) {
     cluster_count = length(unique(geo_so@meta.data[[cluster_column]]))
     
     pc_res_cluster = pc_res_cluster %>%
-      add_row(num_pcs = pc, resolution = res, cluster_count=cluster_count)
+      add_row(included_pcs = pc, resolution = res, cluster_count=cluster_count, umap_filename=umap_file)
     
-    # DimPlot(geo_so,
-    #         group.by = cluster_column,
-    #         label = TRUE,
-    #         reduction = 'umap.integrated.sct.rpca') +
-    #   NoLegend() +
-    #   labs(title=paste(name_suffix, ':', cluster_count, 'clusters')) +
-    #   theme(axis.title=element_blank(),
-    #         axis.text=element_blank(),
-    #         axis.ticks=element_blank(),
-    #         axis.line = element_blank())
-    # 
-    # ggsave(filename = umap_file,
-    #        width = 8, height = 7, units = 'in')
-    
-    # data = get_data(geo_so, 
-    #                 group.by = cluster_column,
-    #                 reduction = 'umap.integrated.sct.rpca')
-    # write_csv(data, file = sprintf('results/tables/%s.csv', name_suffix))
+    geo_so = RunUMAP(geo_so,
+                     dims = 1:pc,
+                     reduction = 'integrated.sct.rpca',
+                     reduction.name = 'umap.integrated.sct.rpca')
+    clustered_umap = DimPlot(geo_so,
+            group.by = cluster_column,
+            label = TRUE,
+            label.size=10,
+            reduction = 'umap.integrated.sct.rpca') +
+      NoLegend() +
+      labs(title=paste(cluster_count, 'clusters')) +
+      theme(axis.title=element_blank(),
+            axis.text=element_blank(),
+            axis.ticks=element_blank(),
+            axis.line = element_blank())
+    ggsave(plot = clustered_umap,
+           filename = sprintf('%s/%s', output_dir, umap_file),
+           width = 8, height = 7, units = 'in')
+
+    data = get_data(geo_so,
+                    group.by = cluster_column,
+                    reduction = 'umap.integrated.sct.rpca')
+    write_csv(data, file = sprintf('%s/%s.csv', output_dir, name_suffix))
+
+
+    # Generate a single "blank" umap for each PC
+    if (res == min(resolutions)) {
+        res = 0
+        name_suffix = sprintf('res=%s_pcs=%s', res, pc)
+        umap_file = sprintf('%s.png', name_suffix)
+
+        blank_umap = DimPlot(geo_so,
+                         reduction = 'umap.integrated.sct.rpca',
+                         cols=rep("#444444", length(unique(Idents(geo_so))))) +
+        NoLegend() +
+        theme(axis.title=element_blank(),
+              axis.text=element_blank(),
+              axis.ticks=element_blank(),
+              axis.line = element_blank())
+
+        ggsave(plot = blank_umap,
+               filename = sprintf('%s/%s', output_dir, umap_file),
+               width = 8, height = 7, units = 'in')
+    }
   }
 }
 
-write_csv(pc_res_cluster, '~/pc_res_cluster.csv')
+write_csv(pc_res_cluster, sprintf('%s/pc_res_cluster.csv', output_dir))
 
-pc_res_cluster$num_pcs = factor(pc_res_cluster$num_pcs)
 
-pc_res_cluster %>% 
-  ggplot(aes(x=resolution, y=cluster_count, group=num_pcs, color=num_pcs)) +
-    geom_line() +
-    theme_minimal()
-  
+# -------------------------------------------------------------------------
+# generate the line plot of clusters x resolution across PCs
+
+# I know we just wrote this file, but writing it and reading it in separate
+# steps makes it easier to tweak this with a new coordinates ore revise the 
+# aesthetics without having to rerun the whole cross-product or res x PCs.
+pc_res_cluster = read_csv(sprintf('%s/pc_res_cluster.csv', output_dir))
+
+pc_res_cluster$included_pcs_f = factor(pc_res_cluster$included_pcs)
+pc_res_cluster$resolution_f = factor(pc_res_cluster$resolution)
+
+
+pc_res_cluster_plt = pc_res_cluster %>% 
+  ggplot(aes(x= included_pcs_f, y=cluster_count, group=resolution_f, color=resolution_f)) +
+  geom_line() +
+  geom_point(size=3) +
+  theme_minimal(base_size=15) +
+  labs(x='Number PCs', y='Number of clusters', color = "Resolution") + 
+  guides(color = guide_legend(reverse=TRUE))
+
+
+pc_res_cluster_plt
+
+ggsave(filename = sprintf('%s/pc_res_cluster.png', output_dir),
+       plot = pc_res_cluster_plt,
+       width = 12, height = 3, units = 'in')
+
+
+#----------------------------------------------------------
+# This is similar to above but switches PCs and resolution.
+# Nice, but *quite* as clear as above.
+#
+# res_pc_cluster_plt = pc_res_cluster %>% 
+#   ggplot(aes(x=resolution_f, y=cluster_count, group=included_pcs_f, color=included_pcs_f)) +
+#     geom_line() +
+#     geom_point(size=3) +
+#     theme_minimal(base_size=15) +
+#     labs(x='Resolution', y='Number of clusters', color = "Included PCs")
+
+#ggsave(filename = sprintf('%s/res_pc_cluster.png', output_dir),
+#       plot = pc_res_cluster_plt,
+#       width = 10, height = 3, units = 'in')
+
+
